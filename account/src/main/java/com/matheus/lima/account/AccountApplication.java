f64265e9c51adbae351df5c44a7887cf7503874b
matheushenriquelima <mateus.matheus.lima@gmail.com>
2021-11-16 21:18:45 -0300

    adiciona anotações

--- account/src/main/java/com/matheus/lima/account/AccountApplication.java
+++ account/src/main/java/com/matheus/lima/account/AccountApplication.java
@@ -2,10 +2,10 @@ package com.matheus.lima.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class AccountApplication {
 
     public static void main(String[] args) {
